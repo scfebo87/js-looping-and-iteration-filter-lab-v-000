df289710a2c6dbfeebe118aa4921db27fc9c2b4e
Sean Christopher Febo <scf@techie.com>
2018-07-16 22:11:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file
+function findMatching(array, string) { 
+return array.filter(name => name === string || name === string.toLowerCase());
+}
+
+function fuzzyMatch(array, string) { 
+  return array.filter(name => name.slice(0, string.length) === string);
+}
+
+function matchName
