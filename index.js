7560453ffc8a1a7e43dc2e9166527aabe6b3910b
Sean Christopher Febo <scf@techie.com>
2018-07-16 22:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ function fuzzyMatch(array, string) {
   return array.filter(name => name.slice(0, string.length) === string);
 }
 
-function matchName
+function matchName(array, string) { 
+  return array.filter()
